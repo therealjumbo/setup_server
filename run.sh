7a3735a69ad6ddd32c6e0d507f4afc21c7aa8c9b
Jeff Zignego <jlzignego@gmail.com>
2018-05-01 22:30:55 -0500

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+#1/bin/bash
+set -e
+
+# ask for password upfront
+sudo -v
+
+# keep-alive; update existing sudo time stamp if set, otherwise do nothing
+while true;
+    do sudo -n true;
+    sleep 60;
+    kill -0 "$$" || exit;
+done 2>/dev/null &
+
+./install/install.sh || exit 1
+
+./setup/setup.sh || exit 1
